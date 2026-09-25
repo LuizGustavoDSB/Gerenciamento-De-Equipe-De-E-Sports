36a75fad3c9ec54ff11f033699d59449e3f778a9
Luiz Gustavo de Souza Bittencourt <ziul666x@gmail.com>
2026-09-25 13:04:43 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -12,29 +12,79 @@ public class Main {
 
         do {
             Menu.menu();
-            opcaoMenu = leitor.nextInt();
 
-            for(Jogador jogadores : listaDeJogadores){
-                System.out.println(jogadores);
+            while(!leitor.hasNextInt()){
+                System.out.println("\nDigite uma das opções.");
+                Menu.menu();
+                leitor.next();
             }
+            opcaoMenu = leitor.nextInt();
+            leitor.nextLine();
 
             switch(opcaoMenu) {
 
                 case 1:
-                    System.out.print("Diga seu nickanme: ");
-                    String nickname = leitor.next();
+                    System.out.println("\n===== CRIANDO PERFIL DE PLAYER =====");
+                    System.out.print("Diga seu nickname: ");
+                    String nickname = leitor.nextLine();
 
-                    System.out.println("Diga seu nome: ");
-                    String nome = leitor.next();
+                    System.out.print("\nDiga seu nome: ");
+                    String nome = leitor.nextLine();
 
-                    System.out.println("Digite sua posição: ");
-                    String posicao = leitor.next();
+                    Menu.posicao();
+                    while (!leitor.hasNextInt()) {
+                        System.out.println("\nDigite um número dentre as opções: ");
+                        Menu.posicao();
+                        leitor.next();
+                    }
+                    int posicao = leitor.nextInt();
 
-                    System.out.println("Digite seu número:");
+                    while (posicao < 1 || posicao > 6) {
+                        System.out.println("\nDigite um número dentre as opções: ");
+                        Menu.posicao();
+
+                        while (!leitor.hasNextInt()) {
+                            System.out.print("\nDigite um número dentre as opções: ");
+                            Menu.posicao();
+                            leitor.next();
+                        }
+                        posicao = leitor.nextInt();
+                        String posicaoTexto = Menu.obterNomePosicao(posicao);
+
+                    }
+                    System.out.print("\nDigite seu número: ");
+
+                    while (!leitor.hasNextInt()) {
+                        System.out.println("\nDigite um número positivo ou mais.");
+                        leitor.next();
+                    }
                     int numero = leitor.nextInt();
 
-                    Jogador jogadorNovo = new Jogador (nickname, nome, posicao, numero);
+                    while (numero <= 0) {
+                        System.out.println("\nEscolha um número positivo.");
+                        System.out.print("Digite seu número: ");
+
+                        while (!leitor.hasNextInt()) {
+                            System.out.println("\nDigite um número positivo ou mais.");
+                            leitor.next();
+                        }
+
+                        numero = leitor.nextInt();
+                    }
+                    Jogador jogadorNovo = new Jogador(nickname, nome, posicao, numero);
                     listaDeJogadores.add(jogadorNovo);
+
+                    System.out.println("\nJogador adicionado com sucesso.");
+
+                    break;
+
+                case 2:
+                    Jogador.listarJogadores(listaDeJogadores);
+
+                    break;
+
+
+
 
 
 
@@ -42,7 +92,7 @@ public class Main {
             }
 
         } while(opcaoMenu !=0);
-        System.out.println("Saindo do sistema.");
+        System.out.println("\nSaindo do sistema...");
 
     }
 }
